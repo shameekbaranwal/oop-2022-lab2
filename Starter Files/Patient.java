c8205ccf93114719594ecc5bb1859232e800d19e
HappyKid117 <65497434+HappyKid117@users.noreply.github.com>
2022-02-11 12:09:53 +0530

    Update Patient.java

--- Starter Files/Patient.java
+++ Starter Files/Patient.java
@@ -4,46 +4,25 @@ public class Patient {
 	String[] symptoms;
 	Disease disease;
 
-	/** Creates a new instance of Patient */
 	Patient(String name, int category, String[] symptoms) {
 
 	}
 
-	/**
-	 * @return the name
-	 */
 	String getName() {
 	}
 
-	/**
-	 * @return the Symptoms
-	 */
 	String[] getSymptoms() {
 	}
 
-	/**
-	 * @return the ID of the patient
-	 */
 	int getID() {
 	}
 
-	/**
-	 * @return the disease
-	 */
 	Disease getDisease() {
 	}
 
-	/**
-	 * Set the disease of the patient
-	 * @param A disease object
-	 *          
-	 */
 	void setDisease(Disease d) {
 	}
 
-	/**
-	 * @return true if the patient has been diagnosed
-	 */
 	boolean hasBeenDiagnosed() {
 	}
 
